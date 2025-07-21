b49670b7044cf52821b586f9097b9f4cfeaa8ccb
moniz-gustavo <moniz_gustavo@outlook.com>
2025-07-21 14:58:12 -0300

    first

---
+++ main.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+  const projetos = [
+    {
+      inf: 'clique para visualizar',  
+      imagem: 'img/javascript.png',  
+      nome: 'Quality Board',
+      descricao: 'Quadro de ações onde é possivel visualizar graficamente as atividades de sua equipe',
+    },
+    {
+      inf: 'clique para visualizar',
+      imagem: 'img/cypress.png',
+      nome: 'Project Name',
+      descricao: 'Description project',
+    },
+    {
+      inf: 'clique para visualizar',
+      imagem: 'img/cypress.png',
+      nome: 'Project Name',
+      descricao: 'Description project',
+    },
+    {
+      inf: 'clique para visualizar',
+      imagem: 'img/cypress.png',
+      nome: 'Project Name',
+      descricao: 'Description project',
+    },
+        {
+      inf: 'clique para visualizar',
+      imagem: 'img/cypress.png',
+      nome: 'Project Name',
+      descricao: 'Description project',
+    },
+    {
+      inf: 'clique para visualizar',
+      imagem: 'img/cypress.png',
+      nome: 'Project Name',
+      descricao: 'Description project',
+    },
+    
+  ];
+
+  const container = document.querySelector('.cartoes'); // ← ponto antes do nome da classe
+
+  projetos.forEach(proj => {
+    const card = document.createElement('div');
+    card.className = 'cartao';
+    card.innerHTML = `
+        <div class="imagem-informativo">
+            <img src="${proj.imagem}" alt="${proj.nome}" class="icone-projeto">
+            <p class="informativo">${proj.inf}</p>
+        </div>
+    <h3>${proj.nome}</h3>
+    <p>${proj.descricao}</p>`;
+    container.appendChild(card); // ← aqui o nome correto da variável
+  });
+};
